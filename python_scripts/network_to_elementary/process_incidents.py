f89fbec0fa3ce1d2af7296f2a729cb4fbe6e1b29
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-05-11 18:15:05 +0800

    partially addresses #29

--- python_scripts/network_to_elementary/process_incidents.py
+++ python_scripts/network_to_elementary/process_incidents.py
@@ -1,4 +1,6 @@
 import time
+import warnings
+import networkx
 import pandas as pd
 import matplotlib.patches
 import osmnx as ox
@@ -15,6 +17,7 @@ from pyproj import Geod
 from shapely.geometry import Point, LineString
 import sys
 import taxicab as tc
+from tqdm import tqdm
 
 
 sys.path.insert(0, "/Users/nishant/Documents/GitHub/WCS/python_scripts/network_to_elementary")
@@ -63,7 +66,8 @@ def create_bbox_to_CCT(
 
     path_missing_counter = 0
     path_present_counter = 0
-    for i in range(incident_data.shape[0]):
+
+    for i, tq in zip(range(incident_data.shape[0]), tqdm(range(incident_data.shape[0]))):
         if not use_route_path:
             bbox_intersecting = line_to_bbox_list(
                 bbox_list,
@@ -74,9 +78,54 @@ def create_bbox_to_CCT(
         else:
             try:
 
-                route_linestring = tc.distance.shortest_path(
-                    graph_with_edge_travel_time, (o_lat[i], o_lon[i]), (d_lat[i], d_lon[i])
-                )
+                with warnings.catch_warnings():
+                    warnings.filterwarnings(
+                        "ignore", message="Geometry is in a geographic CRS. Results from 'distance'"
+                    )
+                    route = tc.distance.shortest_path(
+                        graph_with_edge_travel_time, (o_lat[i], o_lon[i]), (d_lat[i], d_lon[i])
+                    )
+                x_main, y_main = [], []
+                x_first, y_first = [], []
+                x_last, y_last = [], []
+                if route[1]:
+                    # for the meaning of route[1], route[2], route[3]
+                    # see taxicab repository readMe. it is straighforward
+                    #  three parts: one main, and two extensions
+                    for u, v in zip(route[1][:-1], route[1][1:]):
+                        # if there are parallel edges, select the shortest in length
+                        data = min(graph_with_edge_travel_time.get_edge_data(u, v).values(), key=lambda d: d["length"])
+                        if "geometry" in data:
+                            # if geometry attribute exists, add all its coords to list
+                            xs, ys = data["geometry"].xy
+                            x_main.extend(xs)
+                            y_main.extend(ys)
+                        else:
+                            # otherwise, the edge is a straight line from node to node
+                            x_main.extend(
+                                (graph_with_edge_travel_time.nodes[u]["x"], graph_with_edge_travel_time.nodes[v]["x"])
+                            )
+                            y_main.extend(
+                                (graph_with_edge_travel_time.nodes[u]["y"], graph_with_edge_travel_time.nodes[v]["y"])
+                            )
+
+                # process partial edge first one
+                if route[2]:
+                    x_first, y_first = zip(*route[2].coords)
+
+                # process partial edge last one
+                if route[3]:
+                    x_last, y_last = zip(*route[3].coords)
+
+                lon_list = x_main + list(x_first) + list(x_last)
+                lat_list = y_main + list(y_first) + list(y_last)
+
+                XYcoord = (np.column_stack((lat_list, lon_list)).flatten()).tolist()
+                # at this point, we should get lat, lon, lat, lon, ......  and so on
+
+                assert len(XYcoord) % 2 == 0
+                route_linestring = LineString(list(zip(XYcoord[0::2], XYcoord[1::2])))
+
                 bbox_intersecting = line_to_bbox_list(
                     bbox_list,
                     [o_lat[i], o_lon[i], d_lat[i], d_lon[i]],
@@ -85,9 +134,16 @@ def create_bbox_to_CCT(
                     route_linestring=route_linestring,
                 )
                 path_present_counter += 1
-            except:
-                print("Route not found! ")
+            except (UnboundLocalError, IndexError, networkx.exception.NetworkXNoPath) as e:
+                print("Route not found! Straight line used ")
                 path_missing_counter += 1
+                bbox_intersecting = line_to_bbox_list(
+                    bbox_list,
+                    [o_lat[i], o_lon[i], d_lat[i], d_lon[i]],
+                    plotting_enabled=False,
+                    use_route_path=False,
+                    route_linestring=None,
+                )
                 continue
 
         for bbox in bbox_intersecting:
@@ -96,13 +152,21 @@ def create_bbox_to_CCT(
             else:
                 dict_bbox_to_CCT[bbox] = [pd.to_timedelta(incident_data["lasting_time"][i]).total_seconds()]
 
-        if use_route_path:
-            print("Route not found: ", path_missing_counter * 100 / (path_missing_counter + path_present_counter), "%")
+        # if use_route_path:
+        #     print(
+        #         "Route not found counter: ",
+        #         path_missing_counter * 100 / (path_missing_counter + path_present_counter),
+        #         "%",
+        #     )
+    pass
 
     if use_route_path:
         print(
-            "Total route not found: ", path_missing_counter * 100 / (path_missing_counter + path_present_counter), "%"
+            "Outside loop: Total route not found: ",
+            path_missing_counter * 100 / (path_missing_counter + path_present_counter),
+            "%",
         )
+        time.sleep(5)
 
     return dict_bbox_to_CCT
 
