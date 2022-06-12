4ad30ace0c1945ec8cdfcfc10f7a5947b997104d
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-06-12 17:58:38 +0800

    reset order to lon,lat and list of list of list #59

--- python_scripts/network_to_elementary/create_input_for_merge.py
+++ python_scripts/network_to_elementary/create_input_for_merge.py
@@ -1,5 +1,6 @@
 import pickle
 from shapely import geometry
+import numpy as np
 
 
 def get_bbox(osm_tiles_stats_dict):
@@ -22,7 +23,8 @@ def get_bbox_as_list_of_list(scale):
 
     bbox_lol = []
     for lat1, lon1, lat2, lon2 in bbox_list:
-        bbox_lol.append([lat1, lon1, lat2, lon2])
+        # revert for homogeiniety in gdal
+        bbox_lol.append([[lon1, lat1], [lon2, lat2]])
     return bbox_list
 
 
@@ -38,7 +40,7 @@ def create_hierarchy_dict(base_level, number_of_hierarchy):
     return dict_bbox
 
 
-def get_islands_bboxes_for_best_fit_hierarchy(bbox_list):
+def get_isl_and_seeds_bboxes_for_best_fit_hierarchy(bbox_list):
     # two handmade polygons
     poly_1 = [
         [103.86955261230469, 1.3439853145503564],
@@ -76,9 +78,9 @@ def get_islands_bboxes_for_best_fit_hierarchy(bbox_list):
             ]
         )
         if poly_1.intersects(bb_poly):
-            island_1.append([lat1, lon1, lat2, lon2])
+            island_1.append([[lon1, lat1], [lon2, lat2]])
         if poly_2.intersects(bb_poly):
-            island_2.append([lat1, lon1, lat2, lon2])
+            island_2.append([[lon1, lat1], [lon2, lat2]])
 
     # The keys of these three dictionaries are the same
     # read the list of bbox in each island in the best-fit hierarchy
@@ -88,8 +90,15 @@ def get_islands_bboxes_for_best_fit_hierarchy(bbox_list):
     dict_islands["island_1"] = island_1
     dict_islands["island_2"] = island_2
 
-    return dict_islands
+    # seed file:
+    # read start-seed (bbox) in each island in the best-fit hierarchy
+    # dict_seeds = {'island_1':[], 'island_2':[], 'island_3':[], 'island_4':[]}
+    dict_seeds = {}
 
+    dict_seeds["island_1"] = island_1[int(len(island_1) * np.random.rand())]
+    dict_seeds["island_2"] = island_1[int(len(island_2) * np.random.rand())]
+
+    return dict_islands
 
 
 if __name__ == "__main__":
@@ -99,7 +108,7 @@ if __name__ == "__main__":
         pickle.dump(dict_bbox, f, protocol=4)
 
     best_fit_hierarchy = 2
-    dict_islands = get_islands_bboxes_for_best_fit_hierarchy(dict_bbox["hierarchy_" + str(best_fit_hierarchy)])
+    dict_islands = get_isl_and_seeds_bboxes_for_best_fit_hierarchy(dict_bbox["hierarchy_" + str(best_fit_hierarchy)])
     with open("dict_islands_" + str(best_fit_hierarchy) + "_.pickle", "wb") as f:
         pickle.dump(dict_bbox, f, protocol=4)
 
