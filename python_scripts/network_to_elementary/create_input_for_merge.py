5109d4f44e5cfcc82854a4dd1497a178a45bccd9
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-06-12 18:04:12 +0800

    fixed pickle filenames #59

--- python_scripts/network_to_elementary/create_input_for_merge.py
+++ python_scripts/network_to_elementary/create_input_for_merge.py
@@ -98,18 +98,23 @@ def get_isl_and_seeds_bboxes_for_best_fit_hierarchy(bbox_list):
     dict_seeds["island_1"] = island_1[int(len(island_1) * np.random.rand())]
     dict_seeds["island_2"] = island_1[int(len(island_2) * np.random.rand())]
 
-    return dict_islands
+    return dict_islands, dict_seeds
 
 
 if __name__ == "__main__":
     base_level = 5
     dict_bbox = create_hierarchy_dict(base_level, 4)
-    with open("dict_bbox_" + str(base_level) + "_.pickle", "wb") as f:
-        pickle.dump(dict_bbox, f, protocol=4)
 
     best_fit_hierarchy = 2
-    dict_islands = get_isl_and_seeds_bboxes_for_best_fit_hierarchy(dict_bbox["hierarchy_" + str(best_fit_hierarchy)])
-    with open("dict_islands_" + str(best_fit_hierarchy) + "_.pickle", "wb") as f:
+    dict_islands, dict_seeds = get_isl_and_seeds_bboxes_for_best_fit_hierarchy(
+        dict_bbox["hierarchy_" + str(best_fit_hierarchy)]
+    )
+
+    with open("dict_bbox_" + str(base_level) + "_.pickle", "wb") as f:
         pickle.dump(dict_bbox, f, protocol=4)
+    with open("dict_islands_" + str(best_fit_hierarchy) + "_.pickle", "wb") as f:
+        pickle.dump(dict_islands, f, protocol=4)
+    with open("dict_seeds_" + str(best_fit_hierarchy) + "_.pickle", "wb") as f:
+        pickle.dump(dict_seeds, f, protocol=4)
 
     do_nothing = True
