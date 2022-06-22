d5466616e2d7195014fdc6940c5b9237a3d8439d
NISHANT KUMAR <abc.nishant007@gmail.com>
2022-06-22 13:04:30 +0800

    Added file name correctios

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 intermediate_files_path = "/Users/nishant/Documents/GitHub/WCS/intermediate_files/"
-combined_incidents_file = "combined_incidents_sample.csv"
+combined_incidents_file = "combined_incidents_45_days.csv"
 outputfolder = "/Users/nishant/Documents/GitHub/WCS/output_folder/"
 temp_files = "/Users/nishant/Documents/GitHub/WCS/temp_files/"
 
@@ -9,12 +9,12 @@ custom_filter = None  # '["highway"~"motorway|motorway_link|primary"]'
 # '["highway"~"motorway|motorway_link|primary"]'
 #'["highway"~"motorway"]'
 
-
-num_threads = 6
+num_threads = 7
 base = 5
 hierarchies = 6
-best_fit_hierarchy = 4
+best_fit_hierarchy = 5
 use_route_path_curved = False
 plotting_enabled = False
 
 bbox_split_for_merge_optimisation = 8
+color_levels = 89
